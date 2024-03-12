53ece10f0896c59fd8c0ec7382948243ade530c7
AnaLuizaDuck <162720001+AnaLuizaDuck@users.noreply.github.com>
2024-03-12 11:22:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/736x/18/f5/85/18f585aa0edd2fe4e0c98b3aa6fc904f.jpg";
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/474x/4a/a2/64/4aa2648671f805dd01816d3c3bfa5f4c.jpg";
+}
